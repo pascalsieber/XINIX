791973957aca88f224f55ec9cd0e7fee4af82aa5
fueg <fueg@bf31eb18-5c70-cf42-b1ea-85157b9a5c17>
2014-07-15 16:47:17 +0000

    committing batch scripts to start application in windows environment (one for debug, one without debug)
    git-svn-id: https://perseus.zhaw.ch:8443/svn/projects/cis/PinkElefant/pews_reloaded@6989 bf31eb18-5c70-cf42-b1ea-85157b9a5c17

---
+++ src/main/resources/scripts/pews4windows.bat
@@ -0,0 +1,25 @@
+@echo off
+
+cd ..
+set pews_home=%CD%
+
+cd %pews_home%/lib
+set lib=%CD%
+
+set pews_home_prop=-Dch.zhaw.iwi.cis.pews.pewsHome=%pews_home%
+
+set loggin_prop=-Djava.util.logging.config.file=%pews_home%\conf\logging.properties
+
+setlocal ENABLEDELAYEDEXPANSION
+if defined CLASSPATH (set CLASSPATH=%CLASSPATH%;.) else (set CLASSPATH=)
+FOR /R %lib% %%G IN (*.jar) DO set CLASSPATH=!CLASSPATH!;%%G
+
+set CLASSPATH=%CLASSPATH:~1%
+
+::echo PEWS_HOME: %pews_home%
+::echo LIB: %lib%
+::echo PEWS_HOME_PROP: %pews_home_prop%
+::echo LOGGING_PROP: %loggin_prop%
+::echo CLASSPATH: %CLASSPATH%
+
+java -classpath %classpath% %pews_home_prop% %loggin_prop% ch.zhaw.iwi.cis.pews.framework.ZhawEngine
