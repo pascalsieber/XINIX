e88e9486848958f4f5cc961479cfc4b310ab9401
stpn <stpn@bf31eb18-5c70-cf42-b1ea-85157b9a5c17>
2014-08-18 07:39:10 +0000

    fixed bug, where workshop/findAll gives back WorkflowElementImpl (WorkshopImpl and ExerciseImpl) instead of only WorkshopImpl objects. getWorkshopObjectDao was not overridden in WorkshopServiceImpl.
    git-svn-id: https://perseus.zhaw.ch:8443/svn/projects/cis/PinkElefant/pews_reloaded@7143 bf31eb18-5c70-cf42-b1ea-85157b9a5c17

--- src/main/java/ch/zhaw/iwi/cis/pews/service/impl/WorkshopServiceImpl.java
+++ src/main/java/ch/zhaw/iwi/cis/pews/service/impl/WorkshopServiceImpl.java
@@ -1,12 +1,28 @@
 package ch.zhaw.iwi.cis.pews.service.impl;
 
+import ch.zhaw.iwi.cis.pews.dao.WorkshopDao;
+import ch.zhaw.iwi.cis.pews.dao.WorkshopObjectDao;
+import ch.zhaw.iwi.cis.pews.dao.impl.WorkshopDaoImpl;
 import ch.zhaw.iwi.cis.pews.framework.ManagedObject;
 import ch.zhaw.iwi.cis.pews.framework.ManagedObject.Scope;
 import ch.zhaw.iwi.cis.pews.framework.ManagedObject.Transactionality;
+import ch.zhaw.iwi.cis.pews.framework.ZhawEngine;
 import ch.zhaw.iwi.cis.pews.service.WorkshopService;
 
 @ManagedObject( scope = Scope.THREAD, entityManager = "pews", transactionality = Transactionality.TRANSACTIONAL )
 public class WorkshopServiceImpl extends WorkflowElementServiceImpl implements WorkshopService
 {
+	private WorkshopDao workshopDao;
+	
+	public WorkshopServiceImpl()
+	{
+		workshopDao = ZhawEngine.getManagedObjectRegistry().getManagedObject( WorkshopDaoImpl.class.getSimpleName() );
+	}
+	
+	@Override
+	protected WorkshopObjectDao getWorkshopObjectDao()
+	{
+		return workshopDao;
+	}
 
 }
