41270923d36f62610b7a3fb46deb2a540f964ec5
fueg <fueg@bf31eb18-5c70-cf42-b1ea-85157b9a5c17>
2014-09-10 11:42:49 +0000

    added owner to returned result of service find exercise data by exercise ID. TODO: import persistent collection fix and remove ugly code!!
    git-svn-id: https://perseus.zhaw.ch:8443/svn/projects/cis/PinkElefant/pews_reloaded@7266 bf31eb18-5c70-cf42-b1ea-85157b9a5c17

--- src/main/java/ch/zhaw/iwi/cis/pews/dao/impl/ExerciseDataDaoImpl.java
+++ src/main/java/ch/zhaw/iwi/cis/pews/dao/impl/ExerciseDataDaoImpl.java
@@ -8,6 +8,7 @@ import ch.zhaw.iwi.cis.pews.framework.ManagedObject.Scope;
 import ch.zhaw.iwi.cis.pews.framework.ManagedObject.Transactionality;
 import ch.zhaw.iwi.cis.pews.model.WorkshopObject;
 import ch.zhaw.iwi.cis.pews.model.data.ExerciseDataImpl;
+import ch.zhaw.iwi.cis.pews.model.user.UserImpl;
 import ch.zhaw.iwi.cis.pinkelefant.exercise.data.XinixData;
 
 @ManagedObject( scope = Scope.THREAD, entityManager = "pews", transactionality = Transactionality.TRANSACTIONAL )
@@ -23,13 +24,19 @@ public class ExerciseDataDaoImpl extends WorkshopObjectDaoImpl implements Exerci
 	@Override
 	public List< ExerciseDataImpl > findByExerciseID( String exerciseID )
 	{
-		List< ExerciseDataImpl > data = getEntityManager().createQuery( "from ExerciseDataImpl d where d.workflowElement.id = '" + exerciseID + "'" ).getResultList();
+		// TODO remove ugly null setting once persistent collection fix implemented
+		List< ExerciseDataImpl > data = getEntityManager().createQuery( "from ExerciseDataImpl d LEFT JOIN FETCH d.owner where d.workflowElement.id = '" + exerciseID + "'" ).getResultList();
 		getEntityManager().clear();
 
 		for ( ExerciseDataImpl element : data )
 		{
 			element.setWorkflowElement( null );
-			element.setOwner( null );
+			element.getOwner().setSession( null );
+			element.getOwner().setSessionAcceptances( null );
+			element.getOwner().setSessionExecutions( null );
+			element.getOwner().setSessionInvitations( null );
+			((UserImpl)element.getOwner()).setGroups( null );
+			
 
 			// to avoid lazy loading exception, we set owner of xinix image which is part of
 			// xinix data object to null
