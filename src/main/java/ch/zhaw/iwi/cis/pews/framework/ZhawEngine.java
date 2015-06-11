9767138d68c7decc6fd342c217a3bfcdbecf839b
fueg <fueg@zhaw.ch>
2015-06-11 18:41:01 +0200

    preconfigures 20 participants with own asynch session for SBB workshop

--- src/main/java/ch/zhaw/iwi/cis/pews/framework/ZhawEngine.java
+++ src/main/java/ch/zhaw/iwi/cis/pews/framework/ZhawEngine.java
@@ -1639,7 +1639,7 @@ public class ZhawEngine implements LifecycleObject
 			sessionService.start( sessionID );
 		}
 		
-		for ( int i = 0; i < 2; i++ )
+		for ( int i = 0; i < 20; i++ )
 		{
 			String sessionID = sessionService.persist( new SessionImpl(
 				"Asynchrone Session für Teilnehmer " + i,
