41708f1f084090b1a905627d4157b9b80c9c3797
fueg <fueg@zhaw.ch>
2015-12-02 15:48:58 +0100

    modified PosteInput to include posterImages and posterVideos

--- src/main/java/ch/zhaw/iwi/cis/pews/model/input/PosterInput.java
+++ src/main/java/ch/zhaw/iwi/cis/pews/model/input/PosterInput.java
@@ -1,22 +1,31 @@
 package ch.zhaw.iwi.cis.pews.model.input;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import ch.zhaw.iwi.cis.pews.model.instance.ExerciseImpl;
 
 public class PosterInput extends Input
 {
 	private String title;
 	private String description;
+	private Set< String > posterImages;
+	private Set< String > posterVideos;
 
 	public PosterInput()
 	{
 		super();
+		posterImages = new HashSet< String >();
+		posterVideos = new HashSet< String >();
 	}
 
-	public PosterInput( ExerciseImpl exercise, String title, String description )
+	public PosterInput( ExerciseImpl exercise, String title, String description, Set< String > posterImages, Set< String > posterVideos )
 	{
 		super( exercise );
 		this.title = title;
 		this.description = description;
+		this.posterImages = posterImages;
+		this.posterVideos = posterVideos;
 	}
 
 	public String getTitle()
@@ -37,6 +46,26 @@ public class PosterInput extends Input
 	public void setDescription( String description )
 	{
 		this.description = description;
+	}
+
+	public Set< String > getPosterImages()
+	{
+		return posterImages;
+	}
+
+	public void setPosterImages( Set< String > posterImages )
+	{
+		this.posterImages = posterImages;
+	}
+
+	public Set< String > getPosterVideos()
+	{
+		return posterVideos;
+	}
+
+	public void setPosterVideos( Set< String > posterVideos )
+	{
+		this.posterVideos = posterVideos;
 	}
 
 }
