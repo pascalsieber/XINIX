cfb6fd6a790f2d012d40eab6ee2228aa0ad75a15
unknown <fueg@CLT-MOB-W-9461.zhaw.ch>
2016-09-01 11:50:32 +0200

    closes issue #167.

--- src/main/java/ch/zhaw/iwi/cis/pews/service/impl/InvitationServiceImpl.java
+++ src/main/java/ch/zhaw/iwi/cis/pews/service/impl/InvitationServiceImpl.java
@@ -1,7 +1,5 @@
 package ch.zhaw.iwi.cis.pews.service.impl;
 
-import java.util.List;
-
 import ch.zhaw.iwi.cis.pews.PewsConfig;
 import ch.zhaw.iwi.cis.pews.dao.InvitationDao;
 import ch.zhaw.iwi.cis.pews.dao.SessionDao;
@@ -17,51 +15,51 @@ import ch.zhaw.iwi.cis.pews.framework.ZhawEngine;
 import ch.zhaw.iwi.cis.pews.model.instance.SessionImpl;
 import ch.zhaw.iwi.cis.pews.model.user.Invitation;
 import ch.zhaw.iwi.cis.pews.model.user.PrincipalImpl;
+import ch.zhaw.iwi.cis.pews.model.user.UserImpl;
 import ch.zhaw.iwi.cis.pews.service.InvitationService;
 import ch.zhaw.iwi.cis.pews.service.WorkshopService;
 import ch.zhaw.iwi.cis.pews.service.util.MailService;
 import ch.zhaw.iwi.cis.pews.service.util.impl.MailServiceImpl;
 import ch.zhaw.iwi.cis.pinkelefant.workshop.instance.PinkElefantWorkshop;
 
+import java.util.List;
+
 @ManagedObject( scope = Scope.THREAD, entityManager = "pews", transactionality = Transactionality.TRANSACTIONAL )
 public class InvitationServiceImpl extends WorkshopObjectServiceImpl implements InvitationService
 {
-	private InvitationDao invitationDao;
-	private UserDao userDao;
-	private SessionDao sessionDao;
-	private MailService mailService;
+	private InvitationDao   invitationDao;
+	private UserDao         userDao;
+	private SessionDao      sessionDao;
+	private MailService     mailService;
 	private WorkshopService workshopService;
 
 	public InvitationServiceImpl()
 	{
-		invitationDao = ZhawEngine.getManagedObjectRegistry().getManagedObject( InvitationDaoImpl.class.getSimpleName() );
+		invitationDao = ZhawEngine.getManagedObjectRegistry()
+				.getManagedObject( InvitationDaoImpl.class.getSimpleName() );
 		userDao = ZhawEngine.getManagedObjectRegistry().getManagedObject( UserDaoImpl.class.getSimpleName() );
 		sessionDao = ZhawEngine.getManagedObjectRegistry().getManagedObject( SessionDaoImpl.class.getSimpleName() );
 		mailService = ZhawEngine.getManagedObjectRegistry().getManagedObject( MailServiceImpl.class.getSimpleName() );
-		workshopService = ZhawEngine.getManagedObjectRegistry().getManagedObject( WorkshopServiceImpl.class.getSimpleName() );
+		workshopService = ZhawEngine.getManagedObjectRegistry()
+				.getManagedObject( WorkshopServiceImpl.class.getSimpleName() );
 	}
 
-	@Override
-	protected WorkshopObjectDao getWorkshopObjectDao()
+	@Override protected WorkshopObjectDao getWorkshopObjectDao()
 	{
 		return invitationDao;
 	}
 
-	@Override
-	public Invitation findInvitationByID( String id )
+	@Override public Invitation findInvitationByID( String id )
 	{
 		return (Invitation)simplifyOwnerInObjectGraph( findByID( id ) );
 	}
 
-	@SuppressWarnings( "unchecked" )
-	@Override
-	public List< Invitation > findAllInvitations()
+	@SuppressWarnings( "unchecked" ) @Override public List<Invitation> findAllInvitations()
 	{
-		return (List< Invitation >)simplifyOwnerInObjectGraph( findAll() );
+		return (List<Invitation>)simplifyOwnerInObjectGraph( findAll() );
 	}
 
-	@Override
-	public void accept( String invitationID )
+	@Override public void accept( String invitationID )
 	{
 		Invitation invitation = findByID( invitationID );
 		PrincipalImpl user = userDao.findById( invitation.getInvitee().getID() );
@@ -73,20 +71,37 @@ public class InvitationServiceImpl extends WorkshopObjectServiceImpl implements 
 		remove( invitation );
 	}
 
-	@Override
-	public List< Invitation > findByUserID( String userID )
+	@Override public List<Invitation> findByUserID( String userID )
 	{
 		return invitationDao.findByUserID( userID );
 	}
 
-	@Override
-	public List< Invitation > findBySessionID( String sessionID )
+	@Override public List<Invitation> findBySessionID( String sessionID )
 	{
-		return invitationDao.findBySessionID( sessionID );
+		List<Invitation> results = invitationDao.findBySessionID( sessionID );
+
+		// simplify session, invitee and inviter objects
+		for ( Invitation invitation : results )
+		{
+			SessionImpl session = new SessionImpl();
+			session.setID( invitation.getSession().getID() );
+			invitation.setSession( session );
+
+			UserImpl inviter = new UserImpl();
+			inviter.setID( invitation.getInviter().getID() );
+			inviter.setLoginName( ( (UserImpl)invitation.getInviter() ).getLoginName() );
+			invitation.setInviter( inviter );
+
+			UserImpl invitee = new UserImpl();
+			invitee.setID( invitation.getInvitee().getID() );
+			invitee.setLoginName( ( (UserImpl)invitation.getInvitee() ).getLoginName() );
+			invitation.setInvitee( invitee );
+		}
+
+		return results;
 	}
 
-	@Override
-	public void sendByID( String invitationID )
+	@Override public void sendByID( String invitationID )
 	{
 		Invitation invitation = findByID( invitationID );
 		SessionImpl session = sessionDao.findById( invitation.getSession().getID() );
@@ -94,8 +109,7 @@ public class InvitationServiceImpl extends WorkshopObjectServiceImpl implements 
 		sendInvitation( invitation, session );
 	}
 
-	@Override
-	public void sendBySessionID( String sessionID )
+	@Override public void sendBySessionID( String sessionID )
 	{
 		for ( Invitation invitation : findBySessionID( sessionID ) )
 		{
@@ -103,8 +117,7 @@ public class InvitationServiceImpl extends WorkshopObjectServiceImpl implements 
 		}
 	}
 
-	@Override
-	public void sendByWorkshopID( String workshopID )
+	@Override public void sendByWorkshopID( String workshopID )
 	{
 		for ( SessionImpl session : workshopService.findWorkshopByID( workshopID ).getSessions() )
 		{
@@ -114,6 +127,10 @@ public class InvitationServiceImpl extends WorkshopObjectServiceImpl implements 
 
 	private void sendInvitation( Invitation invitation, SessionImpl session )
 	{
-		mailService.sendInvitation( invitation, ( (PinkElefantWorkshop)session.getWorkshop() ).getEmailText(), PewsConfig.getMailSubjectForInvitation(), PewsConfig.getMailUserName() );
+		mailService.sendInvitation(
+				invitation,
+				( (PinkElefantWorkshop)session.getWorkshop() ).getEmailText(),
+				PewsConfig.getMailSubjectForInvitation(),
+				PewsConfig.getMailUserName() );
 	}
 }
